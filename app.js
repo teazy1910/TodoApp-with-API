6edbe7bed508efc85125627520aa5c550fcc8fa0
teazy1910 <tanja@MBP-von-Tanja.fritz.box>
2023-01-16 22:11:51 +0100

    last changes

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const removeBtn = document.querySelector("#removeBtn");
 const input = document.querySelector("#inputField");
 const todoList = document.querySelector("#list");
 
+const filterDone = document.querySelector("#done");
+const filterAll = document.querySelector("#all");
+const filterOpen = document.querySelector("#open");
+
 let todos = [];
 
 function loadTodos() {
@@ -19,15 +23,23 @@ function renderTodos() {
   todoList.innerHTML = "";
   todos.forEach((todo) => {
     const newLi = document.createElement("li");
+
     const text = document.createTextNode(todo.description);
+
+    if (todo.done === true) {
+      input.checked = true;
+    }
 
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.id = todo.id;
     checkbox.checked = todo.done;
-    newLi.appendChild(checkbox);
 
-    newLi.appendChild(text);
+    newLi.append(checkbox, text);
+
     todoList.appendChild(newLi);
+
+    checkbox.addEventListener("change", update);
   });
 }
 // klick button, new todo ans backend posten,
@@ -51,6 +63,43 @@ submitBtn.addEventListener("click", () => {
       todos.push(newTodoFromApi);
       renderTodos();
     });
+  input.value = "";
+  loadTodos();
+});
+
+function update(event) {
+  const id = event.target.id;
+  const updatedTodo = {
+    description: event.target.parentNode.innerText,
+    done: event.target.checked,
+  };
+
+  fetch("http://localhost:4730/todos/" + id, {
+    method: "PUT",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(updatedTodo),
+  })
+    .then((res) => res.json())
+    .then(() => {
+      loadTodos();
+    });
+}
+
+removeBtn.addEventListener("click", () => {
+  // done todo aus der api laden -> löschen
+
+  todos.forEach((todo) => {
+    const currentId = todo.id;
+    if (todo.done === true) {
+      fetch("http://localhost:4730/todos/" + currentId, {
+        method: "DELETE",
+      })
+        .then((res) => response.json(res))
+        .then(() => {
+          loadTodos();
+        });
+    }
+  });
 });
 
 loadTodos();
